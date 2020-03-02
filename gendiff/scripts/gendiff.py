b109c4ea82d47d5502d4db3e69dc8b57dcc03375
ashpb <ashpb@me.com>
2020-03-02 19:01:52 +1000

    Add init files and fix linter warnings

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,13 +3,14 @@
 
 import argparse
 
+
 def main():
-    """Main"""
     parser = argparse.ArgumentParser(description='Generate diff')
     parser.add_argument('first_file', type=str, nargs=1)
     parser.add_argument('second_file', type=str, nargs=1)
     args = parser.parse_args()
     print(args)
+
 
 if __name__ == '__main__':
     main()
