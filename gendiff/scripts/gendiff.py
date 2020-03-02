7d8a41842c4a4690f615d7341335b24eac2643f3
ashpb <ashpb@me.com>
2020-03-02 19:47:53 +1000

    Add format CLI option

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -6,8 +6,9 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description='Generate diff')
-    parser.add_argument('first_file', type=str, nargs=1)
-    parser.add_argument('second_file', type=str, nargs=1)
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
     print(args)
 
