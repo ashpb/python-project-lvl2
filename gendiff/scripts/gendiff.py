fe74dae4377f34100d50f19e90a3f5d4e7ef251b
ashpb <ashpb@me.com>
2020-03-04 14:56:59 +1000

    Add generate_diff logic for flat JSON files

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,16 +2,17 @@
 """Main program script"""
 
 import argparse
+from gendiff import generate_diff
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Generate diff')
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
-    parser.add_argument('-f', '--format', help='set format of output')
+    parser = argparse.ArgumentParser(description="Generate diff")
+    parser.add_argument("first_file")
+    parser.add_argument("second_file")
+    parser.add_argument("-f", "--format", help="set format of output")
     args = parser.parse_args()
-    print(args)
+    print(generate_diff(args.first_file, args.second_file))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
