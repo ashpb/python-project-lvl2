db190fcfcc4c0359fe77363e75b0d9be616372f2
ashpb <ashpb@me.com>
2020-06-29 13:49:30 +1000

    Add YAML diff test

--- tests/test_main.py
+++ tests/test_main.py
@@ -10,3 +10,14 @@ def test_flat_json_diff():
         )
         == diff
     )
+
+
+def test_flat_yaml_diff():
+    with open("tests/fixtures/flat_diff.txt") as f:
+        diff = f.read()
+    assert (
+        generate_diff(
+            "tests/fixtures/flat_before.yaml", "tests/fixtures/flat_after.yaml"
+        )
+        == diff
+    )
