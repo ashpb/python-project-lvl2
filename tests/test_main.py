c47fc197e36dd26cb24aa883fe20f7f5c6508fbb
ashpb <ashpb@me.com>
2020-09-12 03:10:53 +1000

    Add recursive diff tests and update fixtures

--- tests/test_main.py
+++ tests/test_main.py
@@ -21,3 +21,47 @@ def test_flat_yaml_diff():
         )
         == diff
     )
+
+
+def test_json_diff():
+    with open("tests/fixtures/diff.txt") as f:
+        diff = f.read()
+    assert (
+        generate_diff(
+            "tests/fixtures/before.json", "tests/fixtures/after.json"
+        )
+        == diff
+    )
+
+
+def test_yaml_diff():
+    with open("tests/fixtures/diff.txt") as f:
+        diff = f.read()
+    assert (
+        generate_diff(
+            "tests/fixtures/before.yaml", "tests/fixtures/after.yaml"
+        )
+        == diff
+    )
+
+
+def test_json_diff2():
+    with open("tests/fixtures/diff2.txt") as f:
+        diff = f.read()
+    assert (
+        generate_diff(
+            "tests/fixtures/before2.json", "tests/fixtures/after2.json"
+        )
+        == diff
+    )
+
+
+def test_yaml_diff2():
+    with open("tests/fixtures/diff2.txt") as f:
+        diff = f.read()
+    assert (
+        generate_diff(
+            "tests/fixtures/before2.yaml", "tests/fixtures/after2.yaml"
+        )
+        == diff
+    )
